b3cd9d498cb6db0d45dcc7dbb5a3e9ab3363ba54
Son <son.thai@live.com>
2021-05-05 00:59:50 +0700

    Update order_response support for take_profit_limit type

--- lib/binance/order_response.ex
+++ lib/binance/order_response.ex
@@ -2,11 +2,13 @@ defmodule Binance.OrderResponse do
   defstruct [
     :client_order_id,
     :executed_qty,
+    :iceberg_qty,
     :order_id,
     :orig_qty,
     :price,
     :side,
     :status,
+    :stop_price,
     :symbol,
     :time_in_force,
     :transact_time,
